b1e95d7e6ba184c7f8967062ff18c36cda4e6ddc
Vinoth <vinothhtoniv007@gmail.com>
2022-11-26 16:47:49 +0530

    init

--- WebApiCRUD2/Program.cs
+++ WebApiCRUD2/Program.cs
@@ -16,11 +16,5 @@ namespace WebApiCRUD2
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+       
 }
