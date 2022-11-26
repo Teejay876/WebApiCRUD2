83efc841b527f52e5d89f35aee91c27b4789377c
Teejay876 <117691738+Teejay876@users.noreply.github.com>
2022-11-26 16:54:16 +0530

    Update Program.cs

--- WebApiCRUD2/Program.cs
+++ WebApiCRUD2/Program.cs
@@ -16,7 +16,7 @@ namespace WebApiCRUD2
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
